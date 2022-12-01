2008f6db2eb73e3da02359c855ad124c7b2cd979
Balde641 <62099893+Balde641@users.noreply.github.com>
2022-12-01 12:19:16 +0200

    Initial commit

---
+++ ev_charger/merchant_model.js
@@ -0,0 +1,51 @@
+﻿const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'my_database',
+  password: 'Balde12',
+  port: 5432,
+});
+
+const getMerchants = () => {
+  return new Promise(function(resolve, reject) {
+    pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(results.rows);
+    })
+  }) 
+}
+
+const createMerchant = (body) => {
+  return new Promise(function(resolve, reject) {
+    const { name, email, job, age } = body
+
+    pool.query('INSERT INTO merchants (name, email, job, age) VALUES ($1, $2, $3, $4) RETURNING *', [name, email, job, age], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`A new client has been added: ${JSON.stringify(results.rows[0])}`)
+    })
+  })
+}
+
+const deleteMerchant = (merchantId) => {
+  return new Promise(function(resolve, reject) {
+    const id = parseInt(merchantId)
+
+    pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(`Client deleted with ID: ${id}`)
+    })
+  })
+}
+
+module.exports = {
+  getMerchants,
+  createMerchant,
+  deleteMerchant,
+}
---
+++ react-postgres/src/App.js
@@ -0,0 +1,64 @@
+﻿import React, {useState, useEffect} from 'react';
+function App() {
+  const [merchants, setMerchants] = useState(false);
+  useEffect(() => {
+    getMerchant();
+  }, []);
+
+  function getMerchant() {
+    fetch('http://localhost:8000')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setMerchants(data);
+      });
+  }
+
+  function createMerchant() {
+    let name = prompt('Enter client name');
+    let email = prompt('Enter client email');
+    let job = prompt('Enter client job');
+    let age = prompt('Enter client age');
+    fetch('http://localhost:8000/merchants', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, email, job, age}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+
+  function deleteMerchant() {
+    let id = prompt('Enter client id');
+    fetch(`http://localhost:8000/merchants/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getMerchant();
+      });
+  }
+  return (
+    <div>
+      {merchants ? merchants : 'There is no client data available'}
+      <br />
+      <br />
+      <button onClick={createMerchant} style={{ backgroundColor: 'green', width: '100px', height: '50px', borderRadius: '20%', border: '1px solid blue' }}>Add Client</button>
+      <br />
+      <br />
+      <button onClick={deleteMerchant} style={{ backgroundColor: 'red', width: '100px', height: '50px', borderRadius: '20%', border: '1px solid blue' }}>Delete Client</button>
+    </div>
+  );
+}
+export default App;
